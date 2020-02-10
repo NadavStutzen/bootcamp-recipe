a318370871927faf6fbb6ffe052408d73146f1ce
NadavStutzen <nadstutzen@gmail.com>
2020-02-10 11:05:39 +0200

    added recipe route

---
+++ server/routes/api.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const request = require('request')
+
+
+router.get(`/sanity`,function(req,res){
+    res.send(`OK`)
+})
+
+router.get(`/recipes/:ingredient`,function(req,res){
+    let recipe
+    
+    request.get(`https://recipes-goodness.herokuapp.com/recipes/${req.params.ingredient}`,function(err,result){
+        recipe = JSON.parse(result.body);
+        console.log(recipe);
+         
+        
+    })
+    res.send('ggg')
+})
+
+
+
+
+
+
+module.exports = router
